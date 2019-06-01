451a2dcb9a8de1e56c7267164d959fb83f236c0f
AlexM02 <alex.mainguy44@gmail.com>
2019-06-01 12:12:23 +0200

    Ranger/Séparer

--- MenuController.java
+++
@@ -1,8 +0,0 @@
-package tondeuse.controller;
-
-public class MenuController {
-
-  public MenuController(){
-    
-  }
-}
---
+++ src/MenuController.java
@@ -0,0 +1,16 @@
+package tondeuse.controller;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MenuController implements ActionListener{
+
+  private MenuView menu;
+
+  public MenuController(MenuView menu){
+    this.menu = menu;
+  }
+
+  public void actionPerformed(ActionEvent e){
+
+  }
+}
