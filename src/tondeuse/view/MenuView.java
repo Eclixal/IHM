fa1aef68fbbea302da19789bcb3bbb96effd1355
Eclixal <VrD3Tm63a5Tqbw8dMKP378uiPaA76t8C>
2019-06-03 17:15:55 +0200

    BIG MAJ !

--- src/tondeuse/view/MenuView.java
+++ src/tondeuse/view/MenuView.java
@@ -1,6 +1,8 @@
 package tondeuse.view;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.font.TextAttribute;
 import java.util.*;
 import java.awt.*;
 
@@ -13,15 +15,38 @@ public class MenuView extends JFrame {
   public MenuView() {
     this.setTitle("Tondeuse");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLayout(new FlowLayout());
+
+    Dimension tailleEcran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+    int hauteur = (int)tailleEcran.getHeight();
+    int largeur = (int)tailleEcran.getWidth();
+
+    setSize(largeur/2,hauteur/2);
+    setLocationRelativeTo(null);
+    setLayout(null);
+
+    this.setResizable(false);
+
+    this.getContentPane().setBackground(Color.decode("#E6E6E6"));
 
     this.setPreferredSize(new Dimension(1200, 800));
     this.setMinimumSize(new Dimension(1200, 800));
     this.setMaximumSize(new Dimension(1200, 800));
 
-    this.toggleMenu = new JButton("COUCOU");
+    this.jLabel = new JLabel("Tutoriels");
+    this.jLabel.setFont(new Font("Segoe UI", Font.BOLD, 40));
+    this.jLabel.setBounds((this.getWidth()-165)/2, 10, 165, 40);
+
+    Font font = this.jLabel.getFont();
+    Map<TextAttribute, Object> attributes = new HashMap<>(font.getAttributes());
+    attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+    this.jLabel.setFont(font.deriveFont(attributes));
+
+    this.toggleMenu = new JButton("\uF183");
+    this.toggleMenu.setFont(new Font("Font Awesome 5 Free", Font.PLAIN, 30));
+    this.toggleMenu.setBounds(10,10, 35,35);
 
     this.add(toggleMenu);
+    this.add(jLabel);
 
     this.setVisible(true);
   }
