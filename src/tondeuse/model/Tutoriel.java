dfde132eb4c629b3a2b921bb2a05364aaf234531
Gavidof <sebastien.gavignet22@orange.fr>
2019-06-06 11:11:13 +0200

    JSON

--- src/tondeuse/model/Tutoriel.java
+++ src/tondeuse/model/Tutoriel.java
@@ -16,8 +16,48 @@ public class Tutoriel{
     this.configure(this.fileName);
   }
 
-  public void configure(String fileName) {
-    
+  public void configure(String fileName) throws {
+    // parsing file "JSONExample.json"
+    Object obj = new JSONParser().parse(new FileReader(fileName));
+
+    // typecasting obj to JSONObject
+    JSONObject jo = (JSONObject) obj;
+
+    // getting firstName and lastName
+    String firstName = (String) jo.get("firstName");
+    String lastName = (String) jo.get("lastName");
+
+    System.out.println(firstName);
+    System.out.println(lastName);
+
+    // getting age
+    long age = (long) jo.get("age");
+    System.out.println(age);
+
+    // getting address
+    Map address = ((Map)jo.get("address"));
+
+    // iterating address Map
+    Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+    while (itr1.hasNext()) {
+        Map.Entry pair = itr1.next();
+        System.out.println(pair.getKey() + " : " + pair.getValue());
+    }
+
+    // getting phoneNumbers
+    JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+
+    // iterating phoneNumbers
+    Iterator itr2 = ja.iterator();
+
+    while (itr2.hasNext())
+    {
+        itr1 = ((Map) itr2.next()).entrySet().iterator();
+        while (itr1.hasNext()) {
+            Map.Entry pair = itr1.next();
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+        }
+    }
   }
 
   public QuestionAction getAction(int etape){
